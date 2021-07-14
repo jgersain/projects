baeb1ef80241574c58932cc6a8761d9c483f0b13
jgersain <jhersein@gmail.com>
2021-07-13 21:22:04 -0500

    Add behavior to custom controls

---
+++ simple-video-player/js/index.js
@@ -0,0 +1,48 @@
+const $video = document.querySelector('#media-player');
+
+const $backward = document.querySelector('#backward');
+const $play = document.querySelector('#play');
+const $pause = document.querySelector('#pause');
+const $forward = document.querySelector('#forward');
+const $progress = document.querySelector('#progress');
+
+$backward.addEventListener('click', handleBackward);
+$play.addEventListener('click', handlePlay);
+$pause.addEventListener('click', handlePause);
+$forward.addEventListener('click', handleForward);
+
+$video.addEventListener('loadedmetadata', handleLoaded);
+$video.addEventListener('timeupdate', handleTimeUpdate);
+$progress.addEventListener('input', handleProgress);
+
+function handleBackward() {
+  $video.currentTime -= 3;
+}
+
+function handlePlay() {
+  $play.hidden = true;
+  $pause.hidden = false;
+  $video.play();
+}
+
+function handlePause() {
+  $pause.hidden = true;
+  $play.hidden = false;
+  $video.pause();
+}
+
+function handleForward() {
+  $video.currentTime += 3;
+}
+
+function handleLoaded() {
+  $progress.max = $video.duration;
+}
+
+function handleTimeUpdate() {
+  $progress.value = $video.currentTime;
+}
+
+function handleProgress() {
+  $video.currentTime = $progress.value;
+}
